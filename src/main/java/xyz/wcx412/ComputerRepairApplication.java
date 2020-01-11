f5c26d33a00d89320a12b157a6c3027ba52b5788
yysimple <1449697757@qq.com>
2020-01-11 23:21:40 +0800

    Initial commit

---
+++ src/main/java/xyz/wcx412/ComputerRepairApplication.java
@@ -0,0 +1,13 @@
+package xyz.wcx412;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerRepairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerRepairApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/wcx412/ComputerRepairApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.wcx412;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerRepairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
